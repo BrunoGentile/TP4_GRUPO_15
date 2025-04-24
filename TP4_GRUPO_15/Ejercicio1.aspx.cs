e38346f2d6fb0575fb57c9fb211651f483ec7859
fran-bk <frantigre2000@gmail.com>
2025-04-24 17:31:07 -0300

    Ej01  Linkear Base de Datos.

--- TP4_GRUPO_15/Ejercicio1.aspx.cs
+++ TP4_GRUPO_15/Ejercicio1.aspx.cs
@@ -4,14 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
 
 namespace TP4_GRUPO_15
 {
     public partial class Ejercicio1 : System.Web.UI.Page
     {
+        private const string cadenaConexion = @"Data Source=localhost\SQLEXPRESS;Initial Catalog = Viajes; Integrated Security = True; TrustServerCertificate=True";
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                //Establecer conexion
+                SqlConnection sqlConnection = new SqlConnection(cadenaConexion);
+                sqlConnection.Open();
 
+
+
+
+                sqlConnection.Close();
+
+            }
         }
     }
 }
