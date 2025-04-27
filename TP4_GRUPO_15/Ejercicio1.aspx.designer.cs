d0625ac70cff03cb3deb5e1c868b113c5ac194ea
emilsepericles <emilse.pericles@hotmail.com>
2025-04-27 03:23:46 -0300

    ej1: precio vuelos, rfv ej2: boton inicio, summary validation

--- TP4_GRUPO_15/Ejercicio1.aspx.designer.cs
+++ TP4_GRUPO_15/Ejercicio1.aspx.designer.cs
@@ -96,6 +96,15 @@ namespace TP4_GRUPO_15
         protected global::System.Web.UI.WebControls.DropDownList ddlProvinciaFinal;
 
         /// <summary>
+        /// Control lblMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensaje;
+
+        /// <summary>
         /// Control rfvProvinciaFinal.
         /// </summary>
         /// <remarks>
